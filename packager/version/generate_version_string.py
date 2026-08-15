636a050e58031041d01b768238fdbf26231a2956
stratumadev <stratumst@proton.me>
2026-08-15 21:59:29 +0200

    added osx x64 builds

--- packager/version/generate_version_string.py
+++ packager/version/generate_version_string.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
   # else:
   #   output = '{0}-ZLO7'.format(version_hash)
 
-  output = '{0}-ZLO7'.format('v3.8.56')
+  output = '{0}-ZLO7'.format('v3.9.0')
 
   # Final debug message, mirroring what is used to generate the source file:
   print('Final output: {}'.format(output), file=sys.stderr)
